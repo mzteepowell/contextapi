340d18240ec712fe59b0ac1cebc9f5ebd31b9054
Tarah <tarah.agbokhana@gmail.com>
2021-04-01 21:32:33 -0400

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+
+const Product = props => {
+	const { addItem } = useContext(ProductContext)
+
+	return (
+		<div className="product">
+			<img src={props.product.image} alt={`${props.product.title} book`} />
+
+			<h1 className="title">{props.product.title}</h1>
+
+			<p className="price">${props.product.price}</p>
+
+			<button onClick={() => addItem({ ...props.product, id: Date.now() })}>
+				Add to cart
+			</button>
+		</div>
+	);
+};
+
+export default Product;
